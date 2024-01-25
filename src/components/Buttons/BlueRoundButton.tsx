b35cd9a980e76a0044be5d2738e0645303f28c89
Mathys MEUNIER <mathys.meunier@etu.univ-nantes.fr>
2024-01-25 21:27:14 +0100

    Ajout d'un bouton

--- src/components/Buttons/BlueRoundButton.tsx
+++ src/components/Buttons/BlueRoundButton.tsx
@@ -3,7 +3,8 @@ import { ButtonHTMLAttributes } from "react"
 export default function BlueRoundButton(props: ButtonHTMLAttributes<HTMLButtonElement>) {
     return (
         <a href={require("../../assets/cv/CvStageDevWebMeunierMathys.pdf")} download="CvMeunierMathys" target="_blank">
-            <button {...props} className={(props.className || "") + " bg-blue-600 text-slate-200 rounded-2xl p-3 font-bold"}>
+            <button {...props}
+                className={(props.className || "") + " bg-blue-600 text-slate-200 rounded-2xl p-3 font-bold shadow-lg"}>
                 {props.children}
             </button>
         </a>
