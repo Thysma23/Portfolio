d6b1220132cde6b43c0c3cb62f73c60ccbb6f6e8
Mathys MEUNIER <E211579C@ubuntu.srv-podman-2>
2024-01-25 11:21:29 +0100

    correctif style navBar

--- src/components/NavBar/NavBarIndex.tsx
+++ src/components/NavBar/NavBarIndex.tsx
@@ -6,7 +6,7 @@ import NavButtonMain from "./NavButtonMain"
 export default function NavBarIndex() {
     const location = useLocation()
 
-    return <nav className="bg-slate-700 flex flex-row justify-between px-4">
+    return <nav className="bg-slate-700 flex flex-row justify-between px-4 absolute w-full">
         <NavButtonMain />
         <ul className="flex space-x-4 h-14">
             {routes.map((route, index) =>
