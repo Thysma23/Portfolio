b5e6788191a3922ed06d08b212bf369733ddf2fa
Mathys MEUNIER <mathys.meunier@etu.univ-nantes.fr>
2024-01-25 21:28:16 +0100

    Maj de Welcome

--- src/pages/Home/Welcome.tsx
+++ src/pages/Home/Welcome.tsx
@@ -5,11 +5,10 @@ export default function Welcome() {
         <div className=" text-slate-900 my-auto">
             <p className="text-4xl font-bold">
                 Bonjour,
-            </p>
-            <p className="text-4xl font-bold">
+                <br />
                 je suis <span className="text-slate-200">Mathys Meunier</span>,
             </p>
-            <p className="text-2xl font-medium">
+            <p className="text-xl font-medium">
                 Étudiant en 3<span className="text-sm leading-3">e</span> année de BUT Informatique
             </p>
             <BlueRoundButton className="mt-2">Télécharger mon cv</BlueRoundButton>
