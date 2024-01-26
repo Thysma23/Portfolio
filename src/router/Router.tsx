56f6f547689597a28783c9d60ff4adfc56ee20e9
Mathys <mathys.meunier@univ-nantes.fr>
2024-01-26 19:23:11 +0100

    Fix du nom de Objectives -> ObjectivesIndex

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home/HomeIndex';
 import NavBar from '../components/NavBar/NavBarIndex';
-import Objectives from '../pages/Objectives/Objectives';
+import Objectives from '../pages/Objectives/ObjectivesIndex';
 
 export default function Router() {
   return (
