712e4665ba92eb62e489e3295ed422ad06ded7c9
Mathys <mathys.meunier@univ-nantes.fr>
2024-01-26 19:07:49 +0100

    fix de hauteur des boutons de la NavBar

--- src/components/NavBar/NavButton.tsx
+++ src/components/NavBar/NavButton.tsx
@@ -11,7 +11,7 @@ export default function NavButton({ children, location, link }: NavButtonProps) 
         <li className={"h-full flex justify-center items-center"}>
             <Link className={location.endsWith(link)
                 ? "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium h-10"
-                : "text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"} to={link}>
+                : "text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium h-10"} to={link}>
                 {children}
             </Link>
         </li>)
