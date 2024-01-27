784c696fc432d7ca7cd036e5a47358d7106f156b
Mathys <mathys.meunier@univ-nantes.fr>
2024-01-27 17:39:53 +0100

    ajout de hover au blueRoundButton

--- src/components/Buttons/BlueRoundButton.tsx
+++ src/components/Buttons/BlueRoundButton.tsx
@@ -3,7 +3,7 @@ import { ButtonHTMLAttributes } from "react"
 export default function BlueRoundButton(props: ButtonHTMLAttributes<HTMLButtonElement>) {
     return (
         <button {...props}
-            className={(props.className || "") + " bg-blue-600 text-slate-200 rounded-2xl p-3 font-bold shadow-lg"}>
+            className={(props.className || "") + " bg-blue-600 text-slate-200 rounded-2xl p-3 font-bold shadow-lg hover:shadow-2xl"}>
             {props.children}
         </button>
     )
