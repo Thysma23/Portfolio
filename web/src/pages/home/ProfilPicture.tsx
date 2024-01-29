e57e0baba0dd867b738cf7770b848ee30cdcbcdb
Mathys <mathys.meunier@univ-nantes.fr>
2024-01-29 17:29:03 +0100

    Ajout de license

--- web/src/pages/home/ProfilPicture.tsx
+++ web/src/pages/home/ProfilPicture.tsx
@@ -1,3 +1,20 @@
+/** 
+    Copyright (C) 2023 Mathys Meunier
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You can receive a copy of the GNU General Public License
+    along with this program at "http://mathysmeunier/license".
+*/
+
 export default function ProfilPicture() {
     return (
         <div className="bg-blue-500 p-8 rounded-full h-72 w-72 overflow-hidden flex justify-center my-0 md:my-auto">
--- web/src/router/Router.tsx
+++ web/src/router/Router.tsx
@@ -1,3 +1,20 @@
+/** 
+    Copyright (C) 2023 Mathys Meunier
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You can receive a copy of the GNU General Public License
+    along with this program at "http://mathysmeunier/license".
+*/
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/home/HomeIndex';
 import NavBar from '../components/navBar/NavBarIndex';
