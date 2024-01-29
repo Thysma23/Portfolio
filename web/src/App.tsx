7ec83e45d0bef836d89385689296daf122588350
Mathys MEUNIER <mathys.meunier@etu.univ-nantes.fr>
2024-01-29 09:19:15 +0100

    Changement du background

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,10 +3,8 @@ import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
-    document.body.classList.add("bg-gradient-to-r",
-      "from-gray-400",
-      "via-gray-600",
-      "to-blue-800",
+    document.body.classList.add(
+      "bg-gray-500",
       "h-screen",
       "overflow-x-hidden")
   })
