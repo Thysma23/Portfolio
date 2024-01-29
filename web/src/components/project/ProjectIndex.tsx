3c45b6617c378bb3d455352fcc9cb97d42834369
Mathys MEUNIER <mathys.meunier@etu.univ-nantes.fr>
2024-01-29 09:21:12 +0100

    Finition de la page projet

--- web/src/components/project/ProjectIndex.tsx
+++ web/src/components/project/ProjectIndex.tsx
@@ -1,6 +1,7 @@
 import { Repo } from "../../hooks/useRepos"
-import HardSkill from "../skill/HardSkill"
-import logoAvailable from "../skill/logoAvailable.type"
+import logoAvailable from "../logo/logoAvailable.type"
+import IconeAndName from "../logo/IconeAndName"
+import LogoWithHover from "../logo/LogoWithHover"
 
 type ProjectIndexProps = {
     repo: Repo
@@ -8,16 +9,17 @@ type ProjectIndexProps = {
 
 export default function ProjectIndex({ repo }: ProjectIndexProps) {
     return (
-        <section className={`mt-10  w-full md:w-[45%] 2xl:w-[30%] mr-5 flex flex-col justify-between ${repo.url ? "" : "bg-orange-400"}`}>
-            <a href={repo.url}>
-                <h2 className={`hover:underline w-fit ${repo.url ? "text-blue-600" : "text-gray-700"}`}>{repo.name}</h2>
+        <section className={`mt-10  w-full mr-5 flex flex-col justify-between ${repo.url ? "" : "bg-blue-400"}`}>
+            <a href={repo.url} target="_blank" className={`flex justify-between ${repo.url ? "text-blue-600 hover:underline" : ""}`}>
+                <h2 className={`${repo.url ? "text-blue-600 hover:underline" : "text-gray-700"}`}>{repo.name}</h2>
+                <LogoWithHover type={repo.url ? "github" : "univ-nantes"} />
             </a>
-            <p>{repo.description}</p>
+            <p className="text-gray-700">{repo.description}</p>
             <div className="flex justify-between md:max-xl:flex-col">
-                <HardSkill className="mt-5" skill={repo.language as logoAvailable} />
+                <IconeAndName className="mt-5" link={repo.language as logoAvailable} />
                 <span className="place-self-end md:max-xl:place-self-auto">
-                    <span className="hidden sm:inline-block md:max-xl:mt-2 2xl:max-[1700px]:hidden">Dernière mise à jour :&nbsp;</span>
-                    <span className="inline-block sm:hidden 2xl:max-[1700px]:inline-block">Dernière maj :&nbsp;</span>
+                    <span className="hidden min-[1700px]:inline-block">Dernière mise à jour :&nbsp;</span>
+                    <span className="inline-block sm:hidden md:inline-block min-[1700px]:hidden">Dernière maj :&nbsp;</span>
                     <time dateTime={repo.last_update.toDateString()}>{repo.last_update.toLocaleDateString()}</time>
                 </span>
             </div>
--- web/src/pages/projects/ProjectsIndex.tsx
+++ web/src/pages/projects/ProjectsIndex.tsx
@@ -5,9 +5,9 @@ export default function ProjectsIndex() {
     const repos = useRepos()
 
     return (
-        <main className="pt-16 sm:pt-32 md:pt-16 xl:pt-20 2xl:pt-24 mx-5 sm:mx-16 md:mx-20 lg:mx-32">
-            <h1 className="sm:max-md:hidden">Mes projets publics</h1>
-            <div className="flex flex-wrap">
+        <main className="pt-16 sm:pt-32 md:pt-16 xl:pt-20 2xl:pt-24 mx-20 lg:mx-32">
+            <h1 className="sm:max-md:hidden">Mes projets publics et universitaires</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-5 -mx-16 pb-16">
                 {
                     repos.map((repo, id) => (
                         <Project key={id} repo={repo} />
