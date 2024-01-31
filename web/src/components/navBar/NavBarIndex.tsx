ca089af6675078e3eb98562ab090ab60d6334046
Mathys <mathys.meunier@univ-nantes.fr>
2024-01-31 15:34:34 +0100

    Ajout de responsive à la navBar

--- web/src/components/navBar/NavBarIndex.tsx
+++ web/src/components/navBar/NavBarIndex.tsx
@@ -21,7 +21,7 @@ import NavBarMedium from "./NavBarMedium"
 
 export default function NavBarIndex() {
     return (
-        <nav className="bg-slate-700 flex flex-row justify-between w-full h-14 fixed">
+        <nav className="bg-slate-700 flex flex-row justify-between w-full h-14 fixed z-50">
             <NavButtonMain />
             <NavBarMedium />
             <OpenNavBarSmallButton />
