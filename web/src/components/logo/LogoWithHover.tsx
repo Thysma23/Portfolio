30844bbfbcdf2328ea8dd131da5b07cdb3e6d58f
Mathys <mathys.meunier@univ-nantes.fr>
2024-01-31 15:31:15 +0100

    reformat de LogoWithHover

--- web/src/components/logo/LogoWithHover.tsx
+++ web/src/components/logo/LogoWithHover.tsx
@@ -15,24 +15,25 @@
     along with this program at "http://mathysmeunier/license".
 */
 
-import { ReactComponent as GithubLogo } from "@/assets/icones/github.svg"
-import { ReactComponent as UnivNantesLogo } from "@/assets/icones/univ-nantes.svg"
+import { ReactComponent as GithubLogo } from "@/assets/icones/github.svg";
+import { ReactComponent as UnivNantesLogo } from "@/assets/icones/univ-nantes.svg";
 import { useState } from "react"
-import USERNAME from "@/data/USERNAME.json"
+import USERNAME from "@/data/USERNAME.json";
 
 type ProjectIndexProps = {
-    className?: Element['className'],
-    type: "github" | "univ-nantes",
+    className?: Element['className'];
+    type: "github" | "univ-nantes";
     owner?: {
-        login: string
-        url: string
-        avatar: string
-    }
-}
+        login: string;
+        url: string;
+        avatar: string;
+    };
+};
 
 export default function LogoWithHover({ className, type, owner }: ProjectIndexProps) {
-    const [isHover, setIsHover] = useState(false)
-    const iAmOwner = owner && owner.login === USERNAME
+    const [isHover, setIsHover] = useState(false);
+    const iAmOwner = owner && owner.login === USERNAME;
+
     return (
         <div className={`relative ${className}`} onMouseLeave={() => setIsHover(false)}>
             {isHover && <p className="absolute inline-block w-40 p-3 bottom-[105%] right-0 xl:left-1/2 xl:transform xl:-translate-x-1/2 bg-gray-800 text-slate-300 rounded-md">{type === "github" ?
@@ -40,17 +41,17 @@ export default function LogoWithHover({ className, type, owner }: ProjectIndexPr
                     {iAmOwner ? "Projet personnel,"
                         : owner &&
                         <>
-                            <img src={owner.avatar} alt={`Avatar de ${owner.login}`} className="w-4 h-4 inline-block rounded-full mb-1"/>
+                            <img src={owner.avatar} alt={`Avatar de ${owner.login}`} className="w-4 h-4 inline-block rounded-full mb-1" />
                             <a href={owner.url} target="_blank" rel="noreferrer" className="hover:underline">
                                 Auteur : {owner.login},
                             </a>
                         </>
                     }<br />
-                    Les données sont mises à jour via une api toute les 30 minutes."
+                    Les données sont mises à jour via une api toute les 30 minutes.
                 </> : "Projet Universitaire"}</p>}
             {type === "github"
                 ? <GithubLogo className="w-6 h-6" onMouseEnter={() => setIsHover(true)} />
                 : <UnivNantesLogo className="w-6 h-6" onMouseEnter={() => setIsHover(true)} />}
         </div>
-    )
-}
+    );
+};
