a9cab8219e2094fe6865dfde94db7763b10ed57d
Mathys MEUNIER <mathys.meunier@etu.univ-nantes.fr>
2024-02-15 15:01:04 +0100

    Correction des curseurs sur les boutons de changement de version

--- web/src/components/footer/ToggleVersion.tsx
+++ web/src/components/footer/ToggleVersion.tsx
@@ -4,8 +4,8 @@ export default function ToggleVersion() {
     const { setVersion } = useVersion();
 
     return (
-        <p className="hover:underline" onClick={() => setVersion("full")}>
+        <button className="hover:underline" onClick={() => setVersion("full")}>
             Passez à la version complète
-        </p>
+        </button>
     );
 };
--- web/src/components/navBar2/ToggleVersion.tsx
+++ web/src/components/navBar2/ToggleVersion.tsx
@@ -4,8 +4,8 @@ export default function ToggleVersion() {
     const { setVersion } = useVersion();
 
     return (
-        <div className="mt-auto text-slate-200 mx-auto hover:underline" onClick={() => setVersion("simplified")}>
+        <button className="mt-auto text-slate-200 mx-auto hover:underline" onClick={() => setVersion("simplified")}>
             Passez à la version simplifié
-        </div>
+        </button>
     );
 }
